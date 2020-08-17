dff1cd15989a496030738e4fda835938214fca0c
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-08-17 11:41:16 +0900

    Up sb (#31)
    * Update storybuilder(v0.6)
    
    * Change for storybuilder update
    
    * Fix analyze
    
    * Update v1.1.0 [ci skip]

--- src/config.py
+++ src/config.py
@@ -1,73 +1,42 @@
 # -*- coding: utf-8 -*-
-"""Story config.
-"""
+'''
+Story Config
+============
+'''
 
-
-################################################################
-#
-# Sample:
-#
-# 1) PERSONS
-#       人物簡易設定
-# 2) CHARAS <- duplicated
-# 2) STAGES
-#       舞台基本設定
-# 3) DAYS
-#       年月日設定
-# 4) TIMES
-#       時間帯基本設定
-# 5) ITEMS
-#       小道具設定
-# 6) WORDS
-#       辞書設定
-#
-################################################################
-
-
-PERSONS = (
-        # Tag / 氏,名 / 歳 / 誕生日 / 性別 / 職業 / 呼称 / 紹介
-        ("chisa", "千紗", "西村,千紗", 17,(1,1), "female", "高校二年", "me:私"),
-        ("eisuke", "英輔", "中山,英輔", 17,(1,1), "male", "高校二年", "me:俺"),
-        ("miyo", "美代", "田中,美代", 17,(1,1), "female", "高校二年"),
-        )
-
-AREAS = (
-        # Tag / 名前 / x,y / 備考
-        )
-
-STAGES = (
-        # Tag / 名前 / Area / 紹介
-        ("riverbed", "河川敷"),
-        ("crossroad", "交差点"),
-        )
-
-DAYS = (
-        # Tag / 名前 / 月 / 日 / 年
-        ("first", "付き合い始めの日", 5,31, 2019),
-        ("enjoy1", "楽しい日々１", 7,10, 2019),
-        ("disquiet1", "不穏1", 10,11, 2019),
-        ("disquiet2", "不穏2", 10,25, 2019),
-        ("depart", "別れの日", 11,25, 2019),
-        )
-
-TIMES = (
-        # Tag / 名前 / 時 / 分
-        )
-
-ITEMS = (
-        # Tag / 名前 / 紹介
-        ("line", "ＬＩＮＥ"),
-        )
-
-WORDS = (
-        # Tag / 名前 / 紹介
-        )
-
-RUBIS = (
-        # Base / Rubi / Except / Type
-        )
-
-LAYERS = (
-        # Key / Title / Words
-        )
+ASSET = {
+        "PERSONS": (
+            # (tag / name / full / age (birth) / job / call / info)
+            ("chisa", "千紗", "西村,千紗", 17,(1,1), "female", "高校二年", "me:私"),
+            ("eisuke", "英輔", "中山,英輔", 17,(1,1), "male", "高校二年", "me:俺"),
+            ("miyo", "美代", "田中,美代", 17,(1,1), "female", "高校二年"),
+            ),
+        "STAGES": (
+            # (tag / name / parent / (geometry) / info)
+            ("Riverbed", "河川敷", "Tokyo"),
+            ("Crossroad", "交差点", "Tokyo"),
+            ),
+        "DAYS": (
+            # (tag / name / month / day / year)
+            ("first", "付き合い始めの日", 5,31, 2019),
+            ("enjoy1", "楽しい日々１", 7,10, 2019),
+            ("disquiet1", "不穏1", 10,11, 2019),
+            ("disquiet2", "不穏2", 10,25, 2019),
+            ("depart", "別れの日", 11,25, 2019),
+            ),
+        "TIMES": (
+            # (tag / name / hour / minute)
+            ),
+        "ITEMS": (
+            # (tag / name / cate / info)
+            ),
+        "WORDS": (
+            # (tag / name / cate / info)
+            ("line", "ＬＩＮＥ"),
+            ),
+        "RUBIS": (
+            # (origin / rubi / exclusions / always)
+            ("閊え", "閊《つか》え"),
+            ),
+        }
 
--- src/main.py
+++ src/main.py
@@ -1,72 +1,111 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Main story.
-"""
-## path setting
+'''
+Story: "title"
+'''
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append('storybuilder')
-## public libs
-## local libs
 from storybuilder.builder.world import World
-from storybuilder.builder.writer import Writer
-## assets
-from storybuilder.assets import basic, accessory
-## settings
-from config import PERSONS, AREAS, STAGES, DAYS, TIMES, ITEMS, WORDS, RUBIS, LAYERS
-## local files
-from src.episodes.e1_lastwalk import ep_lastwalk
-from src.episodes.e2_change import ep_change_our
-from src.episodes.e3_enjoy import ep_enjoytime
-from src.episodes.e4_firststep import ep_firststep
+from storybuilder.assets import basic
+from storybuilder.assets import common_rubi
+from config import ASSET
+# import scenes
+from scenes import Riverbed
 
-
-## define alias
-W = Writer
-_ = Writer.getWho()
 
 ################################################################
 #
-#   1. 別れの幅
-#   2. 陰りの幅
-#   3. 楽しみの幅
-#   4. 告白の幅
+#   1. Initialize
+#   2. Story memo
+#   3. Structure    - 1/8: 1K
+#   4. Spec
+#   5. Plot         - 1/4: 2K
+#   6. Scenes
+#   7. Conte        - 1/2: 4K
+#   8. Layout
+#   9. Draft        - 1/1: 8K
 #
 ################################################################
 
 
-## main
-def ch_main(w: World):
-    return w.chapter("main",
-            ep_lastwalk(w),
-            ep_change_our(w),
-            ep_enjoytime(w),
-            ep_firststep(w),
+# Constant
+TITLE = "歩幅"
+MAJOR, MINOR, MICRO = 1, 1, 0
+COPY = "恋愛は歩幅を合わせるようなものかも知れない"
+ONELINE = "高校生の二人は初めて付き合い、そして別れた。それだけの物語"
+OUTLINE = "約9000字の恋愛短編。別れることになってしまった高校生の二人。何故そうなったのかを振り返ってみた"
+THEME = "思いのすれ違い"
+GENRE = "恋愛／青春"
+TARGET = "10-30years"
+SIZE = "8K"
+CONTEST_INFO = "コバルト短編小説新人賞第204回応募作"
+CAUTION = ""
+NOTE = ""
+SITES = ["エブリスタ", "小説家になろう", "ノベルアッププラス", "カクヨム"]
+TAGS = ["恋愛", "女主人公", "青春", "高校生"]
+RELEASED = (8, 1, 2020)
+
+
+# episodes
+def ep1(w: World):
+    return w.episode("１",
+            Riverbed.sc_lastwalk(w),
             )
 
-def create_world():
-    """Create a world.
-    """
-    w = World("歩幅")
-    w.setCommonData()
-    w.setAssets(basic.ASSET)
-    w.setAssets(accessory.ASSET)
-    w.buildDB(PERSONS,
-            AREAS, STAGES, DAYS, TIMES, ITEMS, WORDS,
-            RUBIS, LAYERS)
-    w.setBaseDate(2020)
-    w.setBaseArea("Tokyo")
-    # set textures
-    # w.entryBlock()
-    # w.entryHistory()
-    # w.entryLifeNote()
-    w.setOutline("高校生の二人は別れの時を迎える。何故そうなったのか、自分たちの歩幅の違いがすれ違いを生み出していたのだと気づく")
-    return w
+def ep2(w: World):
+    return w.episode("２",
+            Riverbed.sc_rainy(w),
+            Riverbed.sc_apart(w),
+            )
+
+def ep3(w: World):
+    return w.episode("３",
+            Riverbed.sc_handdistance(w),
+            )
+
+def ep4(w: World):
+    return w.episode("４",
+            Riverbed.sc_confession(w),
+            Riverbed.sc_samestep(w),
+            Riverbed.sc_samestep2(w),
+            Riverbed.sc_samestep3(w),
+            )
 
 
+# chapters
+def ch_main(w: World):
+    return w.chapter("main",
+            ep1(w),
+            ep2(w),
+            ep3(w),
+            ep4(w),
+            )
+
+
+# main
 def main(): # pragma: no cover
-    w = create_world()
-    return w.build(
+    w = World.create_world(f"{TITLE}")
+    w.config.set_version(MAJOR, MINOR, MICRO)
+    w.db.set_from_asset(basic.ASSET)
+    w.db.set_from_asset(common_rubi.ASSET)
+    w.db.set_from_asset(ASSET)
+    # spec
+    w.config.set_copy(f"{COPY}")
+    w.config.set_oneline(f"{ONELINE}")
+    w.config.set_outline(f"{OUTLINE}")
+    w.config.set_theme(f"{THEME}")
+    w.config.set_genre(f"{GENRE}")
+    w.config.set_target(f"{TARGET}")
+    w.config.set_size(f"{SIZE}")
+    w.config.set_contest_info(f"{CONTEST_INFO}")
+    w.config.set_caution(f"{CAUTION}")
+    w.config.set_note(f"{NOTE}")
+    w.config.set_sites(*SITES)
+    w.config.set_taginfos(*TAGS)
+    w.config.set_released(*RELEASED)
+    return w.run(
             ch_main(w),
             )
 
